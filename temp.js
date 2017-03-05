8d8988bcfb629bcd4d9d2e789caebaafc68f89b7
daltonhahn <hahnd11@gmail.com>
2017-03-04 19:13:49 -0600

    added some user session code

--- temp.js
+++ temp.js
@@ -27,7 +27,7 @@ exports.handler = (event, context) => {
         console.log(`INTENT REQUEST`)
 
         switch(event.request.intent.name) {
-          
+
           case "AddSong":
             console.log(event.request.intent.slots.SongName.value)
             var songName = intent.slots.SongName.value;
@@ -47,24 +47,10 @@ exports.handler = (event, context) => {
                 console.info("BODY: " + body);
                 var data = JSON.parse(body);
                 var user_response = data['user-response'];
+                var accessToken = request.sessionDetails.accessToken;
                 var speechOutput = "<speak><p>" + user_response + "</p></speak>";
                 callback({}, buildSpeechletResponse(cardTitle, speechOutput, repromptText, false));
                 });
-
-
-            https.get(endpoint, (response) => {
-              response.on('data', (chunk) => { body += chunk })
-              response.on('end', () => {
-                var data = JSON.parse(body)
-                var viewCount = data.items[0].statistics.viewCount
-                context.succeed(
-                  generateResponse(
-                    buildSpeechletResponse(`Current view count is ${viewCount}`, true),
-                    {}
-                  )
-                )
-              })
-            })
           break;
 
           default:
